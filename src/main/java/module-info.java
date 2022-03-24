dffba3cfe466d1c5a4cac113ac0742c79fbff57a
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-03-24 17:04:19 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment2gc200455715 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment2gc200455715 to javafx.fxml;
+    exports com.example.assignment2gc200455715;
+}
