c637f4c438184f88312ec1148d7e43f4bace6a7d
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-04-08 23:27:00 -0400

    Added icon in the every scenes.

--- src/main/java/com/example/assignment2gc200455715/Main.java
+++ src/main/java/com/example/assignment2gc200455715/Main.java
@@ -3,6 +3,7 @@ package com.example.assignment2gc200455715;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image("https://static.vecteezy.com/system/resources/previews/000/263/066/original/book-icon-vector.jpg"));
         stage.setTitle("Search Books");
         stage.setScene(scene);
         stage.show();
